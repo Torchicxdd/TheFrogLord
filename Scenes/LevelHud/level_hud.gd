c5d635d610d81f4c6e9c8bd3aefa6366a535ae71
David-Parkerr <davidparker0040@gmail.com>
2025-06-28 17:58:24 -0400

    Add board script for placing characters on cell

--- Scenes/LevelHud/level_hud.gd
+++ Scenes/LevelHud/level_hud.gd
@@ -1,6 +1,6 @@
 class_name LevelHud extends Node2D
 
-@onready var sprite_texture = $Sprite2D.texture
+@onready var sprite = $Sprite2D
 
 func _ready() -> void:
 	SignalBus.connect("start_level", Callable(self, "start_level"))
@@ -8,15 +8,15 @@ func _ready() -> void:
 func start_level(level: String) -> void:
 	match level:
 		"1":
-			sprite_texture = load("res://Scenes/LevelHud/level1_hud.png")
+			sprite.texture = load("res://Scenes/LevelHud/level1_hud.png")
 		"2":
-			sprite_texture = load("res://Scenes/LevelHud/level2_hud.png")
+			sprite.texture = load("res://Scenes/LevelHud/level2_hud.png")
 		"3":
-			sprite_texture = load("res://Scenes/LevelHud/level3_hud.png")
+			sprite.texture = load("res://Scenes/LevelHud/level3_hud.png")
 		"4":
-			sprite_texture = load("res://Scenes/LevelHud/level4_hud.png")
+			sprite.texture = load("res://Scenes/LevelHud/level4_hud.png")
 		"5":
-			sprite_texture = load("res://Scenes/LevelHud/level5_hud.png")
+			sprite.texture = load("res://Scenes/LevelHud/level5_hud.png")
 		_:
 			# Load a level texture even if it's wrong
-			sprite_texture = load("res://Scenes/LevelHud/level1_hud.png")
+			sprite.texture = load("res://Scenes/LevelHud/level1_hud.png")
