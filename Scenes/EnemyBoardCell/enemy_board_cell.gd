b5c6ee181e60f5fddbc9cd98032de2d672fdaece
David-Parkerr <davidparker0040@gmail.com>
2025-06-28 19:49:45 -0400

    Add Cell object

---
+++ Scenes/EnemyBoardCell/enemy_board_cell.gd
@@ -0,0 +1,1 @@
+class_name EnemyBoardCell extends Cell
--- Scenes/PlayBoard/play_board.gd
+++ Scenes/PlayBoard/play_board.gd
@@ -4,14 +4,14 @@ const BOARD_WIDTH = 3
 const BOARD_HEIGHT = 4
 var astar_grid = AStarGrid2D.new()
 @onready var userCellNodes = $UserCells.get_children()
-var cells: Array[Cell] = []
+var cells: Array[PlayBoardCell] = []
 
-class Cell:
+class PlayBoardCell:
 	var pos: Vector2i
-	var node: Node2D
-	func _init(node: Node2D, index: int) -> void:
+	var cell: Cell
+	func _init(cell: Cell, index: int) -> void:
 		self.pos = find_pos(index)
-		self.node = node
+		self.cell = cell
 	
 	func find_pos(index) -> Vector2i:
 		var count = 0
@@ -29,17 +29,15 @@ func _ready() -> void:
 	setup_grid()
 
 func setup_cells() -> void:
-	for cell in range(userCellNodes):
-		cells[cell] = Cell.new(userCellNodes[cell], cell)
+	for cell in range(userCellNodes.size()):
+		cells.append(PlayBoardCell.new(userCellNodes[cell], cell))
 
 func setup_grid() -> void:
 	astar_grid.region = Rect2i(0, 0, BOARD_WIDTH, BOARD_HEIGHT)
 	astar_grid.update()
 
-func place_character(cell: Node2D, character: Character) -> void:
+func place_character(cell: Cell) -> void:
 	var cell_index = cells.find(cell)
 	# Cell exists
 	if not cell_index == -1:
 		astar_grid.set_point_solid(cells[cell_index].pos)
-		# Place the character down at the position of cell
-		character.position = cells[cell_index].node.position
