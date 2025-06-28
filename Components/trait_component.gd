20361a8a96a39298cd6ca6084f19eb5d35f73899
Kunyro <kunyro@protonmail.ch>
2025-06-28 14:07:44 -0400

    add trait component to every character

--- Components/trait_component.gd
+++ Components/trait_component.gd
@@ -1,12 +1,12 @@
 class_name TraitComponent extends Node
 
 enum TraitNames {
-    CROAKER,
-    EXPLORER,
-    LOVER,
-    BROTHER,
-    NOMAD,
-    WHEEZER,
+	CROAKER,
+	EXPLORER,
+	LOVER,
+	BROTHER,
+	NOMAD,
+	WHEEZER,
 }
 
-@export var trait_name : TraitNames = TraitNames.CROAKER 
+@export var trait_name : TraitNames = TraitNames.CROAKER
