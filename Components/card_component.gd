80d32b24a40009633122122c6c1600966a54d984
Kunyro <kunyro@protonmail.ch>
2025-06-28 17:42:37 -0400

    add effect for when card is bought

--- Components/card_component.gd
+++ Components/card_component.gd
@@ -2,3 +2,17 @@ class_name CardComponent extends Button
 
 @export var card_name: String = "DefaultCardName"
 @export var cost: int = 1
+
+func _ready():
+	# Connect the button's pressed signal to the _on_pressed function
+	connect("pressed", Callable(self, "_on_pressed"))
+
+# When the button is pressed, it will trigger the buy_card function
+func _on_pressed():
+	buy_card()
+	# After the card has been bought, delete the button
+	queue_free()
+
+func buy_card():
+	print("Card bought: %s for %d" % [card_name, cost])
+	SignalBus.emit_signal("card_bought", card_name, cost)
