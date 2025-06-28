8a4a398164ebb0ce121144988ee041d2de1b26f6
Kunyro <kunyro@protonmail.ch>
2025-06-28 14:00:12 -0400

    add trait component

---
+++ Components/trait_component.gd
@@ -0,0 +1,12 @@
+class_name TraitComponent extends Node
+
+enum TraitNames {
+    CROAKER,
+    EXPLORER,
+    LOVER,
+    BROTHER,
+    NOMAD,
+    WHEEZER,
+}
+
+@export var trait_name : TraitNames = TraitNames.CROAKER 
